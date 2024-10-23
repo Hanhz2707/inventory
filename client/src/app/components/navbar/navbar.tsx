9ae2eec0f360139f2a6852cb03410756aa00960b
Pham Huy Anh <phamhuyanh277@gmail.com>
2024-10-23 14:56:16 +0300

    Adding setting icon and insert a link element

--- client/src/app/components/navbar/navbar.tsx
+++ client/src/app/components/navbar/navbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Bell, Menu, Sun } from "lucide-react";
+import { Bell, Menu, Settings, Sun } from "lucide-react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
@@ -32,12 +33,21 @@ const Navbar = () => {
             </button>
           </div>
           <div className="relative">
-            <Bell />
+            <Bell className="cursor-pointer text-gray-500" size={"24"} />
             <span className="absolute -top-2 -right-2 inline-flex items-center justify-center px-[0.4rem] py-1 text-xs font-semibold leading-none text-red-100 bg-red-400 rounded-full">
               3
             </span>
           </div>
+          {/* Creating a verticle line */}
+          <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
+          <div className="flex items-center gap-3 cursor-pointer">
+            <div className="">image</div>
+            <span>Hanhz</span>
+          </div>
         </div>
+        <Link href={"/settings"}>
+          <Settings className="cursor-pointer text-gray-500" size={"24"} />
+        </Link>
       </div>
     </div>
   );
