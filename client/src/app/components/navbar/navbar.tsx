ab79e959f175e16e29b09473e58f286f8446a0a7
Pham Huy Anh <phamhuyanh277@gmail.com>
2024-12-06 21:16:55 +0200

    Deleting unused file, changing navbar style

--- client/src/app/components/navbar/navbar.tsx
+++ client/src/app/components/navbar/navbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Bell, Menu, Settings, Sun } from "lucide-react";
+import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
 import Link from "next/link";
 import { setIsDarkMode, setIsSidebarCollapsed } from "@/app/redux/state";
 import { useAppDispatch, useAppSelector } from "@/app/redux/types/types";
@@ -37,7 +37,7 @@ const Navbar = () => {
           <input
             type="search"
             placeholder="Write here"
-            className="pl-10 pr-4 py-2 w-50 md:w-80 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
+            className="pl-10 pr-4 py-2 w-50 md:w-60 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
           ></input>
           <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
             <Bell className="text-gray-500" size={"20"} />
@@ -50,7 +50,11 @@ const Navbar = () => {
         <div className="hidden md:flex justify-between items-center gap-5">
           <div>
             <button onClick={toggleDarkMode}>
-              <Sun className="cursor-pointer text-gray-500" size={"24"} />
+              {!isDarkMode ? (
+                <Sun className="cursor-pointer text-gray-500" size={"24"} />
+              ) : (
+                <Moon className="cursor-pointer text-gray-500" size={"24"} />
+              )}
             </button>
           </div>
           <div className="relative">
