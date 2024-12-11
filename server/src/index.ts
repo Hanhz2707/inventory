215ae49b2d834ad4ab5af7f9caf7ca9304f3cb7a
Pham Huy Anh <phamhuyanh277@gmail.com>
2024-12-11 22:33:57 +0200

    Successfully fetching data from SQL , setting up routes for dashboard

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,10 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import helmet from 'helmet';
-import morgan from 'morgan';
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import dashboardRoutes from "./routes/dashboardRoutes";
 
 // Routes import
 
@@ -13,16 +14,14 @@ const app = express();
 
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cors());
 // Routes
-app.get('/hello', (req, res) => {
-  res.send('Hello World testing server deployment');
-});
+app.use("/dashboard", dashboardRoutes);
 
 // Server
 const port = process.env.PORT || 3001;
