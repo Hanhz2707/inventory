41a41ca77ed0315dba5d1738ca359048f83dc87c
Pham Huy Anh <phamhuyanh277@gmail.com>
2024-12-14 13:36:25 +0200

    Creating interface for storing data in redux store, create basic structure of UI for dashboard

--- client/src/app/redux/state/api.ts
+++ client/src/app/redux/state/api.ts
@@ -1,10 +1,58 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export interface Dashboard {
+  popularProdcuts: Product[];
+  salesSummary: SaleSummary[];
+  purchaseSummary: PurchaseSummary[];
+  expenseSummary: ExpenseSummary[];
+  expenseByCategorySummary: ExpenseByCategorySummary[];
+}
+
+export interface Product {
+  productId: number;
+  name: string;
+  price: number;
+  rating: number;
+  stockQuantity: number;
+}
+
+export interface SaleSummary {
+  saleSummaryId: number;
+  totalValue: number;
+  changePercentage: number;
+  date: string;
+}
+
+export interface PurchaseSummary {
+  purchaseSummaryId: number;
+  totalPurchased: number;
+  changePercentage: number;
+  date: string;
+}
+
+export interface ExpenseSummary {
+  expenseSummaryId: number;
+  totalExpenses: number;
+  date: string;
+}
+
+export interface ExpenseByCategorySummary {
+  expenseByCategorySummaryId: number;
+  category: string;
+  amount: string;
+  date: string;
+}
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl:process.env.NEXT_PUBLIC_API_BASE_URL}),
-    reducerPath: "api",
-    tagTypes: [],
-    endpoints: (build) => ({})
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
+  reducerPath: "api",
+  tagTypes: ["Dashboard"],
+  endpoints: (build) => ({
+    getDashboard: build.query<Dashboard, void>({
+      query: () => "/dashboard",
+      providesTags: ["Dashboard"],
+    }),
+  }),
 });
 
-export const {} = api;
+export const { useGetDashboardQuery } = api;
--- client/src/app/redux/state/index.ts
+++ client/src/app/redux/state/index.ts
@@ -1,30 +1,29 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export interface InitialStateType {
-    isSideBarCollapsed: boolean;
-    isDarkMode: boolean;
+  isSideBarCollapsed: boolean;
+  isDarkMode: boolean;
 }
 
 // Setting the initial state of the global slice
 const initialState: InitialStateType = {
-    isSideBarCollapsed: false,
-    isDarkMode: false,
+  isSideBarCollapsed: false,
+  isDarkMode: false,
 };
 
 export const globalSlice = createSlice({
-    name: "global",
-    initialState,
-    reducers: {
-
-        // Creating 2 functions to toggle the sidebar and dark mode
-        setIsSidebarCollapsed: (state, action: PayloadAction<boolean>) => {
-            state.isSideBarCollapsed = action.payload;
-        },
-        
-        setIsDarkMode: (state, action: PayloadAction<boolean>) => {
-            state.isDarkMode = action.payload;
-        },
+  name: "global",
+  initialState,
+  reducers: {
+    // Creating 2 functions to toggle the sidebar and dark mode
+    setIsSidebarCollapsed: (state, action: PayloadAction<boolean>) => {
+      state.isSideBarCollapsed = action.payload;
     },
+
+    setIsDarkMode: (state, action: PayloadAction<boolean>) => {
+      state.isDarkMode = action.payload;
+    },
+  },
 });
 
 export const { setIsSidebarCollapsed, setIsDarkMode } = globalSlice.actions;
