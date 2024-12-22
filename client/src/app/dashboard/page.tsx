7608b431aea522d7450a62f8a92865b5d22cafd4
Pham Huy Anh <phamhuyanh277@gmail.com>
2024-12-22 13:52:44 +0200

    Creating card purchase summary and its charts

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -3,13 +3,15 @@
 import React from "react";
 import CardPopularProduct from "./cardPopularProduct";
 import CardSaleSummary from "./cardSaleSummary";
+import { Card } from "@mui/material";
+import CardPurchaseSummary from "./cardPurchaseSummary";
 
 const Dashboard = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
       <CardPopularProduct />
       <CardSaleSummary />
-      <div className="row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-500"></div>
+      <CardPurchaseSummary />
       <div className="row-span-3 bg-gray-500"></div>
       <div className="md:row-span-1 xl:row-span-2 bg-gray-500"></div>
       <div className="md:row-span-1 xl:row-span-2 bg-gray-500"></div>
