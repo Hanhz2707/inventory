c49115cf991fee04eedb02105530549990c4c864
Pham Huy Anh <phamhuyanh277@gmail.com>
2025-01-03 20:55:48 +0200

    Successfully creating products page with each individual products card

--- client/src/app/products/page.tsx
+++ client/src/app/products/page.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import { useGetProductsQuery } from "../redux/state/api";
 import { SearchIcon } from "lucide-react";
+import { Rating } from "@mui/material";
 
 const Products = () => {
   const [search, setSearch] = useState("");
@@ -28,6 +29,72 @@ const Products = () => {
             onChange={(e) => setSearch(e.target.value)}
           />
         </div>
+      </div>
+
+      {/* Header Bar */}
+      {/* <div className="flex justify-between items-center">
+        <h1 className="text-2xl font-bold">Products</h1>
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded-lg hover:bg-gray-700 focus:ring-2 focus:ring-gray-600 transition-all duration-200"
+        >
+          Add Product
+        </button>
+      </div> */}
+
+      {/* <div className="flex justify-between items-center bg-gray-900 p-4 rounded-md shadow-lg">
+        <h1 className="text-2xl font-extrabold text-white tracking-wide">
+          Products
+        </h1>
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded-lg hover:bg-gray-700 focus:ring-2 focus:ring-gray-600 transition-all duration-200"
+        >
+          Add Product
+        </button>
+      </div> */}
+
+      <div className="flex justify-between items-center p-4 rounded-md ">
+        <h1 className="text-3xl font-extrabold ">Products</h1>
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="px-5 py-2 bg-gray-800 text-white text-sm font-bold uppercase rounded-lg hover:bg-gray-700 focus:ring-2 focus:ring-gray-600 transition-all duration-200"
+        >
+          Add Product
+        </button>
+      </div>
+
+      {/* Products */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 justify-between ">
+        {isLoading ? (
+          <div>Loading....</div>
+        ) : (
+          data.map((product) => (
+            <div
+              key={product.productId}
+              className="bg-white shadow-2xl rounded-lg overflow-hidden"
+            >
+              <div className="w-full h-56 bg-cover bg-center">
+                <img
+                  alt={product.name}
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <div className="p-4">
+                <h1 className="text-gray-900 font-bold text-xl">
+                  {product.name}
+                </h1>
+                <p className="mt-2 text-gray-600 text-sm">
+                  Stock: {product.stockQuantity}
+                </p>
+                <div className="flex justify-between items-center mt-4">
+                  <h1 className="text-gray-700 font-bold">€{product.price}</h1>
+                  <Rating value={product.rating} readOnly />
+                </div>
+              </div>
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
