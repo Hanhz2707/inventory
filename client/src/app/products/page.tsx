3c49779984bc1189720ccad6d69da8fffecbc117
Pham Huy Anh <phamhuyanh277@gmail.com>
2025-01-03 20:31:30 +0200

    Creating search bar for product page

--- client/src/app/products/page.tsx
+++ client/src/app/products/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import { useGetProductsQuery } from "../redux/state/api";
+import { SearchIcon } from "lucide-react";
 
 const Products = () => {
   const [search, setSearch] = useState("");
@@ -14,7 +15,22 @@ const Products = () => {
 
   if (isError || !data) return <div>Error</div>;
 
-  return <div className="mx-auto pb-5 w-full">Products</div>;
+  return (
+    <div className="mx-auto pb-5 w-full">
+      {/* Search Bar */}
+      <div className="mb-6">
+        <div className="flex items-center border-2 border-gray-200 rounded">
+          <SearchIcon className="w-6 h-6 ml-2 text-gray-500 m-2" />
+          <input
+            className="w-full py-2 px-4 rounded bg-white"
+            placeholder="Search your products here..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Products;
