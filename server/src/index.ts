4b4d8c697bd3ffec5932d8d10f8d4807eaf70239
Pham Huy Anh <phamhuyanh277@gmail.com>
2025-01-09 17:10:24 +0200

    Adding config file for server

--- server/src/index.ts
+++ server/src/index.ts
@@ -30,8 +30,8 @@ app.use("/users", userRoutes);
 app.use("/expenses", expenseRoutes);
 
 // Server
-const port = process.env.PORT || 3001;
+const port = Number(process.env.PORT) || 3001;
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port ${port}`);
 });
